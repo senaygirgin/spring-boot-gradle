ae82765fe818dfe893c614c1f5cb5b4e354f05df
Senay Serif Girgin <senay.serif.girgin@ericsson.com>
2023-03-06 21:10:26 +0300

    Spring Boot initial

---
+++ commands.md
@@ -0,0 +1,5 @@
+### Run the application
+`gradle bootRun`
+
+### Curl command to run the service
+`curl localhost:8081`
---
+++ src/test/java/com/sesegi/springbootgradle/SpringBootGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sesegi.springbootgradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
